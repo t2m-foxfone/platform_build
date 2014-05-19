d547a83db63f955ea755ff94c2f03e714ea43310
youlong.jiang <youlong.jiang@t2mobile.com>
2014-05-19 16:56:41 +0800

    Add Mozilla Req patch for ril
    Change-Id: I43b9a1602d6997f545ce6dc5dec67c3578c42e59

--- tools/t2m_buildinfo.sh
+++ tools/t2m_buildinfo.sh
@@ -3,6 +3,7 @@
 echo "ro.product.model=Flame"
 echo "ro.t2m.model.name=Flame"
 echo "ro.t2m.hardware.info=01"
+echo "ro.moz.ril.subscription_control=true"
 echo "ro.moz.ril.0.network_types=gsm,wcdma"
 echo "ro.moz.ril.1.network_types=gsm"
 echo "ro.moz.ril.numclients=2"
